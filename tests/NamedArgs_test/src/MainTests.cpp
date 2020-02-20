9f3dcada4c785ef7676a50357c15a7e1fd172eef
Денис Морозов <morden28@yandex.ru>
2020-02-20 15:04:12 +0300

    Added all tests.

--- tests/NamedArgs_test/src/MainTests.cpp
+++ tests/NamedArgs_test/src/MainTests.cpp
@@ -4,16 +4,57 @@
 
 TEST(NamedArgs, FullConfigure)
 {
+  const size_t      AGE       = 23;
   const std::string NAME      = "John";
   const std::string LAST_NAME = "Doe";
-  const size_t      AGE       = 23;
 
   Tests::ChildClass obj;
-  obj.configure(Arguments::NAME      = NAME,
-                Arguments::AGE       = AGE,
+  obj.configure(Arguments::AGE       = AGE,
+                Arguments::NAME      = NAME,
                 Arguments::LAST_NAME = LAST_NAME);
 
   EXPECT_EQ(AGE,       obj.getAge());
   EXPECT_EQ(NAME,      obj.getName());
   EXPECT_EQ(LAST_NAME, obj.getLastName());
+}
+
+TEST(NamedArgs, OneArgumentConfigure)
+{
+  const std::string NAME = "John";
+
+  Tests::ChildClass obj;
+  obj.configure(Arguments::NAME = NAME);
+
+  EXPECT_EQ(size_t(),      obj.getAge());
+  EXPECT_EQ(NAME,          obj.getName());
+  EXPECT_EQ(std::string(), obj.getLastName());
+}
+
+TEST(NamedArgs, ReverseConfigure)
+{
+  const size_t      AGE       = 23;
+  const std::string NAME      = "John";
+  const std::string LAST_NAME = "Doe";
+
+  Tests::ChildClass obj;
+  obj.configure(Arguments::LAST_NAME = LAST_NAME,
+                Arguments::AGE       = AGE,
+                Arguments::NAME      = NAME);
+
+  EXPECT_EQ(AGE,       obj.getAge());
+  EXPECT_EQ(NAME,      obj.getName());
+  EXPECT_EQ(LAST_NAME, obj.getLastName());
+}
+
+TEST(NamedArgs, AmbiguousConfigure)
+{
+  const size_t      AGE = 23;
+
+  Tests::ChildClass obj;
+  obj.configure(Arguments::AGE = AGE,
+                Arguments::AGE = 24);
+
+  EXPECT_EQ(AGE,           obj.getAge());
+  EXPECT_EQ(std::string(), obj.getName());
+  EXPECT_EQ(std::string(), obj.getLastName());
 }
